16d9ccd78b0cd141c3dddf1fa3d802104daee5ff
Enyer Jesús Espino Medina <20240199@itla.edu.do>
2024-04-30 01:37:17 -0400

    Add project files.

---
+++ colmadocuentas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+Calculareverything calcular= new Calculareverything();
+
+calcular.CalcularSueldo();
+
+
+Calculandobase Sumaproducto = new Calculandobase();
+
+Sumaproducto.Calculandosumaproducto();
+
+//Profesor la clase Calculareverything es un codigo al que le agregue mas cosas.
+
+//pero todo fue con lo aprendido en las dos ultimas clases, echele un ojo espero y llene sus expectativas.
+
+
+
+
